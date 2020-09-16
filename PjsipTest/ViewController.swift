af4e2ec2e0c1fa5e7aa1643f1fffc6d14a3078f5
Paresh Prajapati <paresh.prajapati.sa@gmail.com>
2020-09-16 12:44:15 +0530

    Initial Commit

---
+++ PjsipTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PjsipTest
+//
+//  Created by Paresh Prajapati on 16/09/20.
+//  Copyright © 2020 SolutionAnalysts. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
